a5d1e112a51b436225434d074cf4add530c11e50
RameshKumar Kannan <rameshkumaroff@gmail.com>
2026-01-07 20:57:37 +0530

    Add DailyMiraclePractice import to App component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import Home from '@/pages/Home';
 import About from '@/pages/About';
+import DailyMiraclePractice from './pages/DailyMiraclePractice';
 import Testimonials from '@/pages/Testimonials';
 import Contact from '@/pages/Contact';
 import BookSession from '@/pages/BookSession';
